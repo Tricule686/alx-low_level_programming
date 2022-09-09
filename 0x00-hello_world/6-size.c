f23fa0fcd50d2cfcf2aa79295069867ea95824d7
justice <anunobijustice686@gmail.com>
2022-09-08 19:30:27 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
  *
- * Return Always 0 (Success)
- */
+ * * main - Prints the sizeof many var types
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
 int main(void)
 
 {
@@ -22,8 +27,6 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
-
-	printf("Size of a long int: %d byte(s)\n", sizeof(c));
 
 	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 

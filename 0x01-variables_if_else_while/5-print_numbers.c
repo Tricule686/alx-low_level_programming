d2d74943926e212bf6b0ae32d73f62ac706eccee
justice <anunobijustice686@gmail.com>
2022-09-12 14:25:15 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,33 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
-/* betty style doc for function main goes there */
+#include <stdio.h>
 
 /**
- * *main -  Entry point
- * *
- * * Return: Alwaays 0 (Success)
- * */
-int main(void)
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)}
 {
-	  int n;
-
-	  srand(time(0));
-
-	  n = rand() - RAND_MAX / 2;
-	  /* check for negative numbers */
-		  if (n < 0)
-		  {
-			    printf("%d is negative\n", n);
-			  }
-	  else if (n > 0)
-		  {
-			    printf("%d is positive\n", n);
-			  }
-	  else
-		  {
-			    printf("%d is zero\n", n);
-			  }
-	  return (0);
+	int x;
+	for (x = 0; x <=9; x++)
+	{
+	printf("%d", x);
+	}
+	printf("\n");
+return (0); 
 }

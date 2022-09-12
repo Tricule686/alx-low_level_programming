ec69eaf7ba90f82e7b74ceea40d07675fcb74ea8
justice <anunobijustice686@gmail.com>
2022-09-12 13:10:15 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,23 +11,23 @@
  * */
 int main(void)
 {
-  int n;
+	  int n;
 
-  srand(time(0));
+	  srand(time(0));
 
-  n = rand() - RAND_MAX / 2;
-  /* check for negative numbers */
-  if (n < 0)
-  {
-    printf("%d is negative\n", n);
-  }
-  else if (n > 0)
-  {
-    printf("%d is positive\n", n);
-  }
-  else
-  {
-    printf("%d is zero\n", n);
-  }
-  return (0);
-}}
+	  n = rand() - RAND_MAX / 2;
+	  /* check for negative numbers */
+		  if (n < 0)
+		  {
+			    printf("%d is negative\n", n);
+			  }
+	  else if (n > 0)
+		  {
+			    printf("%d is positive\n", n);
+			  }
+	  else
+		  {
+			    printf("%d is zero\n", n);
+			  }
+	  return (0);
+}

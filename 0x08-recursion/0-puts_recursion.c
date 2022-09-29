7543578e83a7b0b95f9e7efbb8c917baaed192b7
justice <anunobijustice686@gmail.com>
2022-09-28 20:04:59 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,41 +1,16 @@
-* File: 0-puts_recursion.c                                                                                                            
-
- * Auth: sam                                                                                                                            
-
- */                                                                                                                                    
-
-                                                                                                                                       
-
-#include "main.h"                                                                                                                      
-
-                                                                                                                                       
-
-/**                                                                                                                                    
-
- * _puts_recursion - Prints a string, followed by a new line.                                                                          
-
- * @s: The string to be printed.                                                                                                        
-
- */                                                                                                                                    
-
-void _puts_recursion(char *s)                                                                                                          
-
-{                                                                                                                                      
-
-        if (*s)                                                                                                                        
-
-        {                                                                                                                              
-
-                _putchar(*s);                                                                                                          
-
-                _puts_recursion(s + 1);                                                                                                
-
-        }                                                                                                                              
-
-                                                                                                                                       
-
-        else                                                                                                                            
-
-                _putchar('\n');                                                                                                        
-
-}
+#include "main.h" 
+  
+ /** 
+  * _print_rev_recursion - prints a string in reverse 
+  * @s: string to be printed 
+  * 
+  * Return: void 
+  */ 
+ void _print_rev_recursion(char *s) 
+ { 
+         if (*s != '\0') 
+         { 
+                 _print_rev_recursion(s + 1); 
+                 _putchar(*s); 
+         } 
+ }

168078026101b4ac6de6d44a275bae995270e43b
justice <anunobijustice686@gmail.com>
2022-09-30 23:01:22 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+ #include "main.h" 
+  
+ /** 
+     * main - Print the name of the program 
+      * @argc: integer type 
+       * @argv: pointer to char type 
+        * Return: return 0 if successful 
+         */ 
+  
+ int main(int __attribute__((__unused__)) argc, char *argv[]) 
+	 { 
+		          printf("%s\n", argv[0]); 
+		    
+		           return (0); 
+		    }
+
